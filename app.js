978f5afbdc4a7f506e3763415eb5aab846fbebed
Manuel <manuel.lopoli@educa.jcyl>
2021-10-22 12:11:41 +0200

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+        var a = 42;
+        document.write("el valor de la variable es " + typeof a + "<br />");
+        a = "hola";
+        document.write("y ahora adquiere el valor " + typeof a + "<br />");
+        a = true;
+        document.write("y ahora el valor " + typeof a + "<br />");
+        var tel1 = 666232323;
+        var tel2 = '666 23 23 23';
+        document.write(typeof tel1);
+        document.write(tel1);
+        document.write(typeof tel2);
+        document.write(tel2);
+        var y = 1e100;
+        document.write(y);
+        var x = 123 + "hola";
+        document.write(typeof x);
+        document.write(x);
+        var t = "hola" + 156;
+        document.write(typeof t);
+        document.write(t);
+        var u;
+        document.write(typeof u);
+        document.write(u);
